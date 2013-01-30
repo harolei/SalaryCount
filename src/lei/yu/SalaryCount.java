b309339169a8f3eb601292508e8fde47d806e364
harolei <harolei@gmail.com>
2013-01-30 18:31:45 +0800

    #3 [harolei] Wrote the test and the implement of counting the tax of the wage.

---
+++ src/lei/yu/SalaryCount.java
@@ -0,0 +1,38 @@
+package lei.yu;
+
+public class SalaryCount {
+    public void setWorkDays(int workDays) {
+        this.workDays = workDays;
+    }
+
+    private int workDays;
+    private double dailyWage = 200.0;
+
+    public SalaryCount(int workDays){
+        this.workDays = workDays;
+    }
+
+    public double getWage() {
+        return workDays* dailyWage;
+    }
+
+    public double getBonus() {
+        double wage = getWage();
+        return wage * 0.1;
+    }
+
+    public double getTaxOfWage() {
+        double wage = getWage();
+        if(wage > 3500){
+            return (wage-3500)*0.1;
+        }
+        else{
+            return 0;
+        }
+    }
+
+    public double getTaxOfBonus() {
+        double bonus = getBonus();
+        return bonus*0.05;
+    }
+}
---
+++ test/lei/yu/SalaryCountTest.java
@@ -0,0 +1,51 @@
+package lei.yu;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class SalaryCountTest {
+
+    private SalaryCount count;
+    private int workDays;
+
+    @Before
+    public void setUp() throws Exception {
+        workDays = 20;
+        count = new SalaryCount(workDays);
+    }
+
+    @Test
+    public void should_count_wage_when_input_work_days() throws Exception {
+        assertThat(count.getWage(),is(workDays * 200.0));
+    }
+
+    @Test
+    public void should_count_bonus_when_input_work_days() throws Exception {
+        assertThat(count.getBonus(),is(workDays * 200.0 * 0.1));
+    }
+
+    @Test
+    public void should_count_the_tax_of_wage_when_wage_is_more_than_3500() throws Exception {
+        double wage = workDays*200;
+        if(wage >= 3500){
+            assertThat(count.getTaxOfWage(),is((wage-3500)*0.1));
+        }
+    }
+
+    @Test
+    public void should_output_0_when_wage_is_less_than_3500() throws Exception {
+        count.setWorkDays(10);
+        double wage = 10*200;
+        if(wage < 3500){
+            assertThat(count.getTaxOfWage(),is(0.0));
+        }
+    }
+
+    @Test
+    public void should_output_the_tax_of_bonus() throws Exception {
+        assertThat(count.getTaxOfBonus(),is(workDays * 200 *0.1*0.05));
+    }
+}
